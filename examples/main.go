1790e0c5580c6079be4d1d6589d887852b362b4f
Justin <iceyang1007@gmail.com>
2020-05-26 07:19:28 +0800

    fix example

--- examples/main.go
+++ examples/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/iceyang/xgin"
 	"github.com/iceyang/xgin/examples/lib"
@@ -23,16 +22,6 @@ func main() {
 	x.Invoke(func() {
 		fmt.Println("I'm a invoke function")
 	})
-	if err := x.Run(); err != nil {
-		log.Fatalf("[xgin] Start with error: %+v\n", err)
-	}
-	log.Println("[xgin] Running")
 
-	<-x.Done()
-
-	log.Println("[xgin] Stopping")
-	if err := x.Stop(); err != nil {
-		log.Fatalf("[xgin] Stop with error: %+v\n", err)
-	}
-	log.Println("[xgin] Stopped")
+	x.Run()
 }
